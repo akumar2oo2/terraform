daeb0d2534bf7a8ccf02dbf6bffa7c04185f8a0f
akumar2oo2 <ayushkumar.office@gmail.com>
2026-06-10 17:36:44 +0530

    Create locals.tf
    feat: add locals.tf and centralize resource naming configuration

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  resource_group_name = "MyRG"
+  location            = "West Europe"
+
+  # Naming conventions
+  storage_account_name = "akstorage7347379"
+  container_name       = "akcontainer"
+  blob_name            = "ak-main"
+  vnet_name            = "ak-vnet"
+}
